39c3b039d3345dea297aa433ede79b478b87bf0e
Patricio José <soriwert@gmail.com>
2017-01-26 19:23:24 +0100

    sin terminar

--- src/programaHora/ProgramaHora.java
+++ src/programaHora/ProgramaHora.java
@@ -9,22 +9,44 @@ import java.util.*;
 
 class Hora {
 	private int h,m,s;
+	String c="0";
 
 	public Hora (){
-	h=00;
-	m=00;
-	s=00;
-}
-	public int getdimeHora(int h){
+		
+		this.h=0;
+		this.m=0;
+		this.s=0;
+	}
+	
+	public void setHora(int h){
 		this.h=h;	
+	}
+	public void setMin(int m){
+		this.m=m;	
+	}
+	public void setSeg(int s){
+		this.s=s;	
+	}
+	public void setOperacion(int h, int m, int s){
+		this.h=h;
+		this.m=m;
+		this.s=s;
+	}
+	
+	public String getOperacion(int h, int m, int s){
+		this.h=h;
+		this.m=m;
+		this.s=s;
+		return ""+h+":"+m+":"+s+"";
+	}
+	public int getHora(){
 		return h;
 	}
-	public int getdimeMin(int m){
-		this.m=m;
+	
+	public int getMin(){
 		return m;
 	}
-	public int getdimeSeg(int s){
-		this.s=s;
+	public int getSeg(){
 		return s;
 	}
 }
@@ -34,9 +56,50 @@ public class ProgramaHora {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado = new Scanner(System.in);
-		Hora miHora = new Hora ();
+		int c=0, HoraUsu=0, MinUsu=0, SegUsu=0, opcion=0;
 		
-		System.out.print("La hora inicial es: " + miHora.getdimeHora(h)+":"+miHora.getdimeMin(m)+":"+miHora.getdimeSeg(s));
+		
+		Hora miHora = new Hora();
+		
+		
+		do{
+			System.out.println("Menu \nElija una opción: \n1. Hora Predeterminada. \n2. Establecer hora. \n3. Sumar hora. \n4. Restar hora. "
+					+"\n5. Imprimir hora. \n6. Leer hora. \n0. Salir");
+			opcion=teclado.nextInt();
+			switch (opcion){
+//hora predeterminada
+			case 1:
+				System.out.println("La hora inicial es: "+c+""+miHora.getHora()+":"+c+""+miHora.getMin()+":"+c+""+miHora.getSeg());	
+				break;
+// Establecer hora
+			case 2:
+				System.out.println("Escriba la hora: ");
+				HoraUsu=teclado.nextInt();
+				System.out.println("Escriba los minutos: ");
+				MinUsu=teclado.nextInt();
+				System.out.println("Escriba los Segundos");
+				SegUsu=teclado.nextInt();
+				break;
+			case 3:
+				System.out.print(miHora.getOperacion(HoraUsu, MinUsu, SegUsu));
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			}
+			if (opcion<0 || opcion>6){
+				System.out.println("Error, vuelva a escribir un número.");
+			}
+		}while(opcion != 0);
+		
+		
+		
+		
+		
+		
 	}
 
 }
