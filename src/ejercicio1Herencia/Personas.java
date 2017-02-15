f0defd62030fcb3aa55aad5cd34679d59361fd3f
SORIWERT <SORIWERT@DESKTOP-HU5ALIK>
2017-02-15 01:50:01 +0100

    con departamento y titulo null

--- src/ejercicio1Herencia/Personas.java
+++ src/ejercicio1Herencia/Personas.java
@@ -13,18 +13,18 @@ class Empleado{
 		this.nombre=nombre;
 		this.sueldo=sueldo;
 	}
-	
+
 	public Empleado(){
-		
+
 	}
-	
+
 	public void numeroEmpleados(int nEmpleado){
 		this.nEmpleado=nEmpleado;
 	}
 	public int numeroEmpleados(){
 		return nEmpleado;
 	}
-	
+
 	public void estableceNombre(String nombre){
 		this.nombre=nombre;
 	}
@@ -47,22 +47,38 @@ class Empleado{
 
 class Jefe extends Empleado {
 	private String titulo, departamento;
+	int nJefes;
+
 	
-	public Jefe (){
-		
-	}
 	public Jefe(String nombre, int sueldo, String titulo, String departamento, int nEmpleado){
 		super (nombre, sueldo, nEmpleado);
+
+	}
+	public Jefe (){
+		this.nJefes=super.numeroEmpleados();
+	}
 	
-		}
-	
+	public void numeroEmpleados(int nEmpleados){
+		this.nJefes=nEmpleados;
+		this.nJefes=super.numeroEmpleados();
+	}
+
 	public String dameDepartamento(){
 		return departamento;
 	}
 	public String dameTitulo(){
 		return titulo;
 	}
+	
+	public void estableceDepartamento(String departamento){
+		this.departamento=departamento;
+	}
+	public void estableceTitulo(String titulo){
+		this.titulo=titulo;
+	}
 }
+
+
 
 public class Personas {
 
@@ -79,46 +95,56 @@ public class Personas {
 		
 		Empleado[] Empleados=new Empleado[nEmpleados];
 		for (int i=0; i<nEmpleados;i++){
+			
 			System.out.println("Escriba el nombre");
 			nombre=teclado.next();
+			Empleados1.estableceNombre(nombre);
+			
 			System.out.println("Escriba el sueldo:");
 			sueldo=teclado.nextInt();
+			Empleados1.estableceSueldo(sueldo);
 			
-			Empleados[i]=new Empleado(nombre, sueldo, nEmpleados);
+			Empleados[i]=new Empleado(Empleados1.dameNombre(), Empleados1.dameSueldo(), nEmpleados);
 			
 		}
 		
 		
-		Jefe Jefes1 = new Jefe();
+		Jefe jefes1 = new Jefe();
 		
 		System.out.println("Cuantos jefes son?: ");
 		nJefes=teclado.nextInt();
+		jefes1.numeroEmpleados(nJefes);
 		
 		Jefe[] Jefes=new Jefe[nJefes];
+		
+		
 		
 		for(int i=0;i<nJefes;i++){
 		System.out.println("Escriba el nombre del Jefe: ");
 		nombre=teclado.next();
-		
+		jefes1.estableceNombre(nombre);
 		System.out.println("Escriba el sueldo: ");
 		sueldo=teclado.nextInt();
-		
+		jefes1.estableceSueldo(sueldo);
 		System.out.println("Escriba el título: ");
 		titulo=teclado.next();
-		
+		jefes1.estableceTitulo(titulo);
 		System.out.println("Escriba el nombre del departamento: ");
 		departamento=teclado.next();
+		jefes1.estableceDepartamento(departamento);
 		
 		
-		Empleados[i]=new Jefe(nombre, sueldo, titulo, departamento, nEmpleados);
+		Jefes[i]=new Jefe(jefes1.dameNombre(), jefes1.dameSueldo(), jefes1.dameTitulo(), jefes1.dameDepartamento(), nEmpleados);
 		}
 		
 		
-		
-		for (int i=0; i<nEmpleados;i++){
-			System.out.println("Nombre: "+Empleados[i].dameNombre()+" sueldo: "+Empleados[i].dameSueldo());
-			System.out.println("Nombre: "+Jefes[i].dameNombre()+" Sueldo "+Jefes[i].dameSueldo()+" Titulo: "+ Jefes[i].dameTitulo()+" departamento: "+Jefes[i].dameDepartamento());
+		for (Empleado e: Empleados){
+			System.out.println("Nombre: "+e.dameNombre()+" sueldo: "+e.dameSueldo());
 		}
+		for (Jefe j: Jefes){
+			System.out.println("Nombre: "+j.dameNombre()+" Sueldo "+j.dameSueldo()+" Titulo: "+ j.dameTitulo()+" departamento: "+j.dameDepartamento());
+		}
+
 	}
 
 }
